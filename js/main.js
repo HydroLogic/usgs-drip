2e55497b62640913b776c7b5b5f6a36d91c137cf
Ishmael Smyrnow <ismyrnow@appgeo.com>
2015-09-22 15:40:03 -0400

    Simple map of dams with cartodb

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(function () {
   'use strict';
 
-  console.log('Hello, world!');
+  // Let's start with a fullscreen map, for now.
+  cartodb.createVis('map', 'https://appgeo.cartodb.com/u/clientdemos/api/v2/viz/e8829dcc-615f-11e5-8e12-0e4fddd5de28/viz.json');
 });
