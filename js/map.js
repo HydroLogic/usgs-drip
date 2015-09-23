667545a26f8e78cbd4738e4478be2b663b4382a1
Ishmael Smyrnow <ismyrnow@appgeo.com>
2015-09-23 15:09:30 -0400

    Switch to sidebar layout

--- js/map.js
+++ js/map.js
@@ -2,6 +2,10 @@ $(function () {
   'use strict';
 
   var vizUrl = 'https://appgeo.cartodb.com/u/clientdemos/api/v2/viz/e8829dcc-615f-11e5-8e12-0e4fddd5de28/viz.json';
+  var options = {
+    shareable: false,
+    scrollwheel: true
+  };
 
-  cartodb.createVis('map', vizUrl);
+  cartodb.createVis('map', vizUrl, options);
 });
